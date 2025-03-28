f29d9ad99da1576abcc5ec7cc827e1db29ea04fb
Rodrigo Iván Ordoñez Chávez <rodrigoordonez@nube.unadmexico.mx>
2025-03-28 08:13:40 -0600

    modification on navbar and logo

--- src/components/LogoBms.js
+++ src/components/LogoBms.js
@@ -4,41 +4,34 @@ import React from "react";
 
 const Logo = ({ className }) => {
   return (
-    <div className={`flex flex-col text-left w-full ${className}`}>
+    <div
+      className={`flex flex-col text-left ${className}`}
+      style={{ width: "150px" }}
+    >
+      {" "}
+      {/* Ajustar el ancho del contenedor */}
       {/* Primera línea: BMS Support */}
       <div className="flex justify-end w-full">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-2xl font-bold">
+          {" "}
+          {/* Reducir el tamaño del texto */}
           <span style={{ color: "var(--primary)" }}>BMS</span>{" "}
           <span style={{ color: "var(--secondary)" }}>Support</span>
         </h1>
       </div>
       {/* Segunda línea */}
       <div className="flex justify-end w-full">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-2xl font-bold">
+          {" "}
+          {/* Reducir el tamaño del texto */}
           <span style={{ color: "var(--secondary)" }}>Technology</span>
           <span style={{ color: "var(--primary)" }} className="ml-2">
             LLC
           </span>
         </h1>
       </div>
-      {/* Eslogan */}
-      <div className="flex justify-center w-full mt-4">
-        <p
-          style={{ color: "var(--foreground)" }}
-          className="text-sm font-medium text-center max-w-xs"
-        >
-          We Transform Ideas Into Digital Innovation
-        </p>
-      </div>
     </div>
   );
 };
 
 export default Logo;
-
-
-
-
-
-
-
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import Link from "next/link";
 import Logo from "./LogoBms";
 
-
 const Links = [
   { href: "/", Text: "Home" },
   { href: "/information", Text: "Information" },
@@ -19,19 +18,17 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-black text-white py-2 fixed w-full z-20 top-0 left-0 shadow-lg transition-all duration-300">
+    <nav className="bg-black text-white py-4 fixed w-full z-20 top-0 left-0 shadow-lg transition-all duration-300">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logotipo */}
         <div className="flex items-center">
-        <Logo className="w-auto" /> {/* Usar el componente Logo */}
+          <Logo className="w-64 h-16" />{" "}
+          {/* Usar el componente Logo y reducir tamaño */}
         </div>
 
         {/* Menú hamburguesa para pantallas pequeñas */}
         <div className="block lg:hidden">
-          <button
-            onClick={toggleMenu}
-            className="focus:outline-none mr-4"
-          >
+          <button onClick={toggleMenu} className="focus:outline-none mr-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -55,7 +52,7 @@ const NavBar = () => {
             <Link
               key={link.href}
               href={link.href}
-              className="text-white hover:text-green-500 transition-colors duration-300 py-2 px-4 rounded"
+              className="text-white hover:text-green-500 transition-colors duration-300 py-0.5 px-4 rounded"
             >
               {link.Text}
             </Link>
